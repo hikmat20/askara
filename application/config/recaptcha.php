b9ec7706dbd13108ff243f22fb57355d28454b05
hikmat20 <hikmataulia20@gmail.com>
2026-03-24 20:00:45 +0700

    update recaptcha from db

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,8 +1,15 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+$CI = &get_instance();
+
+$setting = $CI->db->get('settings')->result();
+foreach($setting as $set ){
+    $recaptcha[$set->setting_name] = $set->value;
+}
+
 $config['recaptcha'] = array(
-    'site_key'   => '6LeZDZQsAAAAACg53HMyhK3SrSJqBtcUwg9Rhhek',    // Ganti dengan Site Key Anda
-    'secret_key' => '6LeZDZQsAAAAAElgxNR8CSlisD3p7ua43XT32uzY',  // Ganti dengan Secret Key Anda
-    'threshold'  => 0.5        // Ambang batas skor(0.0 - 1.0)
+    'site_key'   => $recaptcha['recaptcha_site_key'],    // Ganti dengan Site Key Anda
+    'secret_key' => $recaptcha['recaptcha_secret_key'],  // Ganti dengan Secret Key Anda
+    'threshold'  => $recaptcha['recaptcha_threshold']      // Ambang batas skor(0.0 - 1.0)
 );
