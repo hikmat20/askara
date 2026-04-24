189e08d3e1e0e417fc21901cc16dc8ffa787f523
hikmat20 <hikmataulia20@gmail.com>
2026-04-24 09:24:30 +0700

    update manage document

--- application/modules/manage_documents/views/view-file.php
+++ application/modules/manage_documents/views/view-file.php
@@ -1,3 +1,3 @@
 <div class="text-center">
-	<iframe src="<?= base_url("directory/$folderMain/$company_id/$parent_name/$file->file_name"); ?>#toolbar=0&navpanes=0" frameborder="0" width="100%" height="500px"></iframe>
+	<iframe src="<?= base_url("directory/$folderMain/$company_id/$file->file_name"); ?>#toolbar=0&navpanes=0" frameborder="0" width="100%" height="500px"></iframe>
 </div>
