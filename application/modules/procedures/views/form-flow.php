6515ec14035cd1fff38b5526d0d6030226ebbff3
hikmat20 <hikmataulia20@gmail.com>
2026-05-20 17:06:32 +0700

    fix bugs procedures

--- application/modules/procedures/views/form-flow.php
+++ application/modules/procedures/views/form-flow.php
@@ -1,4 +1,4 @@
-<input type="hidden" name="procedure_id" value="<?= $procedure_id; ?>">
+<input type="hidden" name="id" value="<?= $procedure_id; ?>">
 <div class="form-group">
 	<label class="">Nomor <span class="text-danger">*</span></label>
 	<div class="">
